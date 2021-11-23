5d8b32003f9922a4eb3c37253af78472ce821955
Kevin4theHuynh <85657180+Kevin4theHuynh@users.noreply.github.com>
2021-11-23 00:08:14 -0800

    Created working PORT

--- Develop/server.js
+++ Develop/server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express()
-
-
+const fs = require('fs')
 
 const useIndexRouter = require('./routes/indexRoutes');
 const useNotesRouter = require('./routes/notesRoutes');
 
 
 app.use("/indexRoutes", useIndexRouter)
-app.use("/notesRoutes", useNotesRouter)
 
-app.listen(3000)
+
+
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
