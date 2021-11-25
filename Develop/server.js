5457664c905b06d30f9a3de2f6ee6cc71729eca3
Kevin4theHuynh <85657180+Kevin4theHuynh@users.noreply.github.com>
2021-11-25 00:06:53 -0800

    Changes

--- Develop/server.js
+++ Develop/server.js
@@ -1,15 +1,41 @@
 const express = require('express');
 const app = express()
 const fs = require('fs')
-
+const path = require('path')
 const useIndexRouter = require('./routes/indexRoutes');
-const useNotesRouter = require('./routes/notesRoutes');
-
-
-app.use("/indexRoutes", useIndexRouter)
+const newNote = require('./db/db.json');
 
 
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'))
+
+
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+})
+
+app.get('/api/notes', (req, res) => {
+    res.json(newNote.slice(1))
+})
+
+function postNote (body, array) {
+    const sendNote = body;
+    if (!Array.isArray(array))
+    array = ['']
+    if (array.length = 0) {
+        let addNote = array.push('')
+    }
+        
+    
+}
+
+app.post('/api/notes', (req, res) => {
+    const addNote 
+})
+
+// app.use("/indexRoutes", useIndexRouter)
 const PORT = process.env.PORT || 4000
-
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
